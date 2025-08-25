344b86b4a6c414982c3a08ae8d57944858f9104b
Mirek R. <mirekreuter@web.de>
2025-08-25 11:09:32 +0200

    BUGGED
    Former-commit-id: 387a698eec0bb73e7ac9142e79a3ea5970c7aa7a

--- src/main/java/lightning/Sun.java
+++ src/main/java/lightning/Sun.java
@@ -32,35 +32,35 @@ public class Sun{
     }
 
     public Matrix4f getLightSpaceMatrix(){
-        Matrix4f lightProjection = new Matrix4f().ortho(-20, 20, -20, 20, 1, 50);
-        Matrix4f lightView = new Matrix4f().lookAt(Position, new Vector3f(0, 0,0), new Vector3f(0, 1, 0));
+        //Matrix4f lightProjection = new Matrix4f().ortho(-20, 20, -20, 20, 1, 50);
+        //Matrix4f lightView = new Matrix4f().lookAt(Position, new Vector3f(0, 0,0), new Vector3f(0, 1, 0));
         Matrix4f lightSpaceMatrix = new Matrix4f();
-        lightProjection.mul(lightView, lightSpaceMatrix);
+        //lightProjection.mul(lightView, lightSpaceMatrix);
         return lightSpaceMatrix;
     }
     public void UpdateShadows()
     {
-        GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, shadowFBO);
-        GL11.glViewport(0, 0, 1024, 1024);
-        GL11.glClear(GL11.GL_DEPTH_BUFFER_BIT);
+        //GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, shadowFBO);
+        //GL11.glViewport(0, 0, 1024, 1024);
+        //GL11.glClear(GL11.GL_DEPTH_BUFFER_BIT);
     }
 
     public void GenerateShadowBuffer()
     {
-        shadowFBO = GL30.glGenFramebuffers();
-        shadowMap = GL11.glGenTextures();
-
-        GL11.glBindTexture(GL11.GL_TEXTURE_2D, shadowMap);
-        GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_DEPTH_COMPONENT, 1024, 1024, 0, GL11.GL_DEPTH_COMPONENT, GL11.GL_FLOAT, (ByteBuffer)null);
-        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
-        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
-        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
-        GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
-
-        GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, shadowFBO);
-        GL32.glFramebufferTexture(GL30.GL_FRAMEBUFFER, GL30.GL_DEPTH_ATTACHMENT, shadowMap, 0);
-        GL11.glDrawBuffer(GL11.GL_NONE);
-        GL11.glReadBuffer(GL11.GL_NONE);
-        GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, 0);
+        //shadowFBO = GL30.glGenFramebuffers();
+        //shadowMap = GL11.glGenTextures();
+//
+        //GL11.glBindTexture(GL11.GL_TEXTURE_2D, shadowMap);
+        //GL11.glTexImage2D(GL11.GL_TEXTURE_2D, 0, GL11.GL_DEPTH_COMPONENT, 1024, 1024, 0, GL11.GL_DEPTH_COMPONENT, GL11.GL_FLOAT, (ByteBuffer)null);
+        //GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MIN_FILTER, GL11.GL_NEAREST);
+        //GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST);
+        //GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_S, GL12.GL_CLAMP_TO_EDGE);
+        //GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_WRAP_T, GL12.GL_CLAMP_TO_EDGE);
+//
+        //GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, shadowFBO);
+        //GL32.glFramebufferTexture(GL30.GL_FRAMEBUFFER, GL30.GL_DEPTH_ATTACHMENT, shadowMap, 0);
+        //GL11.glDrawBuffer(GL11.GL_NONE);
+        //GL11.glReadBuffer(GL11.GL_NONE);
+        //GL30.glBindFramebuffer(GL30.GL_FRAMEBUFFER, 0);
     }
 }
